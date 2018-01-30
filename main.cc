d56065fdd0832acd0a9c6b2e0b96b7cda0815099
Ihor Pilipenko <GrIgar45@gmail.com>
2018-01-30 17:22:56 +0200

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,39 +1,32 @@
-#include <iostream>
-#include <errno.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <wiringPi.h>
 #include <wiringPiI2C.h>
 
-using namespace std;
+#define MPU6050_GYRO_XOUT_H        0x43   // R
+#define MPU6050_GYRO_YOUT_H        0x45   // R
+#define MPU6050_GYRO_ZOUT_H        0x47   // R
+
+#define MPU6050_PWR_MGMT_1         0x6B   // R/W
+#define MPU6050_I2C_ADDRESS        0x68   // I2C
 
 int main()
 {
-   int fd, result;
+    int fd = wiringPiI2CSetup(MPU6050_I2C_ADDRESS);
+    if (fd == -1)
+        return;
 
-   // Initialize the interface by giving it an external device ID.
-   // The MCP4725 defaults to address 0x60.   
-   //
-   // It returns a standard file descriptor.
-   // 
-   fd = wiringPiI2CSetup(0x60);
+    wiringPiI2CReadReg8 (fd, MPU6050_PWR_MGMT_1);
+    wiringPiI2CWriteReg16(fd, MPU6050_PWR_MGMT_1, 0);
 
-   cout << "Init result: "<< fd << endl;
+    float x,y,z;
 
-   for(int i = 0; i < 0x0000ffff; i++)
-   {
-      // I tried using the "fast write" command, but couldn't get it to work.  
-      // It's not entirely obvious what's happening behind the scenes as
-      // regards to endianness or length of data sent.  I think it's only 
-      // sending one byte, when we really need two.
-      //
-      // So instead I'm doing a 16 bit register access.  It appears to 
-      // properly handle the endianness, and the length is specified by the 
-      // call.  The only question was the register address, which is the 
-      // concatenation of the command (010x = write DAC output) 
-      // and power down (x00x = power up) bits.
-      result = wiringPiI2CWriteReg16(fd, 0x40, (i & 0xfff) );
+    while(true)
+    {
+        x = wiringPiI2CReadReg8(fd, MPU6050_GYRO_XOUT_H);
+        y = wiringPiI2CReadReg8(fd, MPU6050_GYRO_YOUT_H);
+        z = wiringPiI2CReadReg8(fd, MPU6050_GYRO_ZOUT_H);
 
-      if(result == -1)
-      {
-         cout << "Error.  Errno is: " << errno << endl;
-      }
-   }
+        printf("x=%d   y=%d   z=%d", x,y,z); 
+    }
 }
