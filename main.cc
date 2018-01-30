b858bf46ac32ae8616cc8d2c8bc6db470d731d20
igor.pilipenko <igor.pilipenko@chisw.com>
2018-01-30 17:33:20 +0200

    update main.cc

--- main.cc
+++ main.cc
@@ -1,32 +1,30 @@
 #include <stdio.h>
-#include <stdint.h>
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
 
-#define MPU6050_GYRO_XOUT_H        0x43   // R
-#define MPU6050_GYRO_YOUT_H        0x45   // R
-#define MPU6050_GYRO_ZOUT_H        0x47   // R
-
-#define MPU6050_PWR_MGMT_1         0x6B   // R/W
-#define MPU6050_I2C_ADDRESS        0x68   // I2C
-
-int main()
+int main (int argc, char *argv[])
 {
-    int fd = wiringPiI2CSetup(MPU6050_I2C_ADDRESS);
-    if (fd == -1)
-        return;
-
-    wiringPiI2CReadReg8 (fd, MPU6050_PWR_MGMT_1);
-    wiringPiI2CWriteReg16(fd, MPU6050_PWR_MGMT_1, 0);
-
-    float x,y,z;
-
-    while(true)
+    int fd;
+    int data;
+    wiringPiSetup () ;
+    fd=wiringPiI2CSetup (0x68) ;  /*Use i2cdetect command to find your respective device address*/
+    if(fd==-1)
     {
-        x = wiringPiI2CReadReg8(fd, MPU6050_GYRO_XOUT_H);
-        y = wiringPiI2CReadReg8(fd, MPU6050_GYRO_YOUT_H);
-        z = wiringPiI2CReadReg8(fd, MPU6050_GYRO_ZOUT_H);
-
-        printf("x=%d   y=%d   z=%d", x,y,z); 
+        printf("Can't setup the I2C device\n");
+        return -1;
+    } else {
+        for (;;)
+        {
+            data=wiringPiI2CRead(fd);
+            if(data==-1)
+            {
+                printf("No data\n");
+                //return -1;
+            } else {
+                //print data
+                printf("data=%d\n", data);
+            }
+        }
     }
+    return 0;
 }
