8ff2a5d2bf6b0103fef5afae46b9df88be66014f
igor.pilipenko <igor.pilipenko@chisw.com>
2018-02-07 19:29:03 +0200

    Write check

--- main.cc
+++ main.cc
@@ -6,16 +6,21 @@
 
 int main(int argc, char *argv[]) {
     wiringPiSetup();
+    std::cout << "Start initialize L3GD20" << std::endl;
     int fd = wiringPiI2CSetup(0x6b);
     if (fd == -1) {
         printf("Can't setup the I2C device\n");
         return -1;
     }
+    std::cout << "Ok. Check reading" << std::endl;
     int data = wiringPiI2CReadReg8(fd, 0x0f);
-    if (data == 0xd4) {
-        std::cout << "Success" << std::endl;
-    } else {
-        std::cout << std::hex << data << std::endl;
+    if (data != 0xd4) {
+        std::cout << "L3GD20 is not working" << std::endl;
+        return 1;
     }
+    delay(10);
+    std::cout << "Ok. Read is finished successfully. Now check writing" << std::endl;
+    data = wiringPiI2CWriteReg8(fd, 0x20, 0x0f);
+    std::cout << std::hex << data << std::endl;
     return 0;
 }
