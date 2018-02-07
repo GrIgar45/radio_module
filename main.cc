d6e91b38496de2a8dbc079a6cde71fa2d9b276d0
igor.pilipenko <igor.pilipenko@chisw.com>
2018-02-07 18:15:33 +0200

    Reformat code

--- main.cc
+++ main.cc
@@ -2,26 +2,19 @@
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
 
-int main (int argc, char *argv[])
-{
-    int fd;
-    int data;
-    wiringPiSetup () ;
-    fd=wiringPiI2CSetup (0x6b) ;  /*Use i2cdetect command to find your respective device address*/
-    if(fd==-1)
-    {
+
+int main(int argc, char *argv[]) {
+    wiringPiSetup();
+    int fd = wiringPiI2CSetup(0x6b);
+    if (fd == -1) {
         printf("Can't setup the I2C device\n");
         return -1;
     } else {
-        for (;;)
-        {
-            data=wiringPiI2CRead(fd);
-            if(data==-1)
-            {
+        for (;;) {
+            static int data = wiringPiI2CRead(fd);
+            if (data == -1) {
                 printf("No data\r");
-                //return -1;
             } else {
-                //print data
                 printf("data=%d\r", data);
             }
         }
