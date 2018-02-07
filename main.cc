c76f87eaf3b00d13ee83ad9b4bb65502506e0885
igor.pilipenko <igor.pilipenko@chisw.com>
2018-02-07 17:23:32 +0200

    changed device address

--- main.cc
+++ main.cc
@@ -7,7 +7,7 @@ int main (int argc, char *argv[])
     int fd;
     int data;
     wiringPiSetup () ;
-    fd=wiringPiI2CSetup (0x3b) ;  /*Use i2cdetect command to find your respective device address*/
+    fd=wiringPiI2CSetup (0x3c) ;  /*Use i2cdetect command to find your respective device address*/
     if(fd==-1)
     {
         printf("Can't setup the I2C device\n");
