0894791ea7616f18d477cfd67cb2471372903e6b
igor.pilipenko <igor.pilipenko@chisw.com>
2018-02-14 14:16:47 +0200

    lol

--- GyroI2C.cpp
+++ GyroI2C.cpp
@@ -102,8 +102,8 @@ std::string GyroI2C::toString() {
 
 std::string GyroI2C::toStringLastData() {
     std::stringstream s;
-    s << "Raw data\n";
     s << std::fixed << std::setprecision(3);
+    s << "Raw data\n";
     s << "X: " << lastData[0];
     s << "\tY: " << lastData[1];
     s << "\tZ: " << lastData[2];
